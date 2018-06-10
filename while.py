826ba66ed610c4b48e465e8bbeca7e9a34406387
andiwijayaid <andiwijayaid@gmail.com>
2018-06-10 20:58:15 +0800

    While syntax

---
+++ while.py
@@ -0,0 +1,20 @@
+nilai = 20;
+
+while nilai <= 30:
+    print(nilai)
+    nilai = nilai + 1
+
+start = True
+angka = 1
+batas = 20
+
+print("")
+print(20*"=")
+print("")
+
+while start:
+    print(angka, "Benar")
+    angka = angka + 1
+
+    if angka is batas:
+        start = False
