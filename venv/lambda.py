c805b2a4ff89881ce961f9082372040b450c746d
andiwijayaid <andiwijayaid@gmail.com>
2018-06-10 21:45:54 +0800

    Function in phyton

---
+++ venv/lambda.py
@@ -0,0 +1,14 @@
+def jumlah(a, b):
+    c = a + b
+    return c
+
+hasil = jumlah(2, 4)
+
+print(hasil)
+
+# Membuat anonymous function dengan lambda
+kali = lambda x, y: x*y
+
+hasil = kali(4,5)
+
+print(hasil)
